1744853e8d4662f85282e4ca0e2b4d97b95e6887
PradeepaBharathi <bharathi83201@gmail.com>
2024-02-20 11:53:32 +0530

    changed

--- src/Utils/APIRoutes.js
+++ src/Utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:9000";
+export const host = "https://chatterbox-une6.onrender.com";
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
